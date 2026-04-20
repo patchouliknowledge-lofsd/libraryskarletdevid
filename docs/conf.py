2c7d2fa08e22395ceae0a2298990ed86e83716bf
Patchouli Knowledge <sasa.nebas4@gmail.com>
2026-04-20 15:06:41 +1000

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'Библиотека влой дьяволицы'
+author = 'Patchouli Knowlege'
+release = '1.0'
+
+language = 'ru'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
